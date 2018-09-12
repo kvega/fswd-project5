467c933885ceb3b914da80e8165ad207072bd690
kvega <kevin.n.vega@gmail.com>
2018-09-11 18:43:04 -0700

    Got map to show up

--- js/app.js
+++ js/app.js
@@ -1,39 +1,23 @@
-var keyLocations = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
-    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
-];
+// Define global variables
+// Map variable
+var map;
+var markers = [];
+var polygon;
 
-var Location = function(data) {
-    this.title = ko.observable(data.title);
-    this.location = ko.observable(data.location);
-};
-
-var LocationsView = function() {
-    var self = this;
-    self.locationFilter = ko.observable();
-    self.locationList = ko.observableArray([]);
-
-    keyLocations.forEach(function(locationItem) {
-        self.locationList.push(new Location(locationItem))
-    });
-
-
-
-};
-
-var MapView = function() {
-
-};
-
+// Create our ViewModel
 var ViewModel = function() {
     var self = this;
 
-    self.locationsView = new LocationsView();
-    self.locationList = self.locationsView.locationList;
+    self.initMap = function() {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 36.970550, lng: -122.015760},
+            zoom: 15
+        });
+    };
 };
 
-ko.applyBindings(new ViewModel());
+function initApp() {
+    var vm = new ViewModel();
+    vm.initMap();
+    ko.applyBindings(vm);
+};
